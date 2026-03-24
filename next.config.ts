78daee9786bcd49042dc2a59150637cc1134b724
stefanuspet <stefanuspetra.p@gmail.com>
2026-03-25 00:53:20 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "*.supabase.co",
+        hostname: "*.public.blob.vercel-storage.com",
       },
     ],
   },
