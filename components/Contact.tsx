3cef5a649978618b1d7ae953cc2abce80b75bee4
stefanuspet <stefanuspetra.p@gmail.com>
2026-04-25 19:43:39 +0700

    add address

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -50,10 +50,16 @@ ${form.message}
       href: "https://instagram.com/swatu.design",
     },
     {
-      label: "Office",
+      label: "Yogyakarta Office",
       value:
         "Bleber Kidul RT 06 RW 17, Sumberharjo, Prambanan, Sleman, DIY 55572",
       href: "https://maps.google.com/?q=Sumberharjo+Prambanan+Sleman",
+    },
+    {
+      label: "Bali Office",
+      value:
+        "Jl. Raya Sukawati No.75, Sukawati, Kec. Sukawati, Kabupaten Gianyar, Bali 80582, Indonesia",
+      href: "https://maps.google.com/?q=Jl.+Raya+Sukawati+No.75+Sukawati+Gianyar+Bali",
     },
   ];
 
